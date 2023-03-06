1fb5ec2c50dd913ed5b3e4ff036931942f29d654
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2023-03-05 22:19:18 -0500

    Simplify test

--- src/test/kotlin/LoadBloscTest.kt
+++ src/test/kotlin/LoadBloscTest.kt
@@ -3,14 +3,15 @@ package com.hanslovsky.jblosc.native
 import org.blosc.JBlosc
 import org.junit.Assert
 import org.junit.Test
+import org.scijava.nativelib.NativeLibraryUtil
 import org.scijava.nativelib.NativeLoader
 import java.io.IOException
 import java.nio.ByteBuffer
 
 class LoadBloscTest {
 
-    @Test fun loadBlosc() = NativeLoader.loadLibrary("blosc")
-    @Test fun failLoadLibrary(): Unit = Assert.assertThrows(IOException::class.java) { NativeLoader.loadLibrary("csolb") }.let {  }
+    @Test fun loadBlosc() = Assert.assertTrue(loadNativeLibrary("blosc"))
+    @Test fun failLoadLibrary(): Unit = Assert.assertFalse(loadNativeLibrary("csolb"))
     @Test fun runBlosc() {
         loadBlosc()
         val blosc = JBlosc()
@@ -23,5 +24,10 @@ class LoadBloscTest {
             ByteBuffer.allocate(1 + JBlosc.OVERHEAD),
             1L + JBlosc.OVERHEAD
         )
+    }
+
+    companion object {
+        val jniExtractor get() = NativeLoader.getJniExtractor()
+        fun loadNativeLibrary(libName: String): Boolean = NativeLibraryUtil.loadNativeLibrary(jniExtractor, libName)
     }
 }
